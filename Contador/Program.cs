001a51210dddcebdcc74767d58cea38cb084a8f0
Thiago Barreto <thiagobarreto0705@GMAIL.COM>
2024-06-26 00:46:23 -0300

    Contador finalizado.

--- Contador/Program.cs
+++ Contador/Program.cs
@@ -1,6 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 
-Start(50);
+using System.ComponentModel.Design;
+
+Menu();
+
+
+static void Menu(){
+  
+  Console.Clear();
+  Console.WriteLine("S - Segundos => 1s = 1 segundo");
+  Console.WriteLine("M - Minutos => 1m = 1 minuto");
+  Console.WriteLine("0 - Sair");
+  Console.WriteLine("Quanto tempo deseja contar ?");
+
+  string data = Console.ReadLine().ToLower();
+  char type = char.Parse(data.Substring(data.Length-1, 1));
+  int time = int.Parse(data.Substring(0, data.Length-1));
+  int multiplier = 1;
+
+  if(type == 'm')
+    multiplier = 60;
+
+  if(data == "0")
+    System.Environment.Exit(0);
+
+  PreStart(time * multiplier);  
+}
+
+static void PreStart(int time){
+  Console.Clear();
+  Console.WriteLine("Preparar...");
+  Thread.Sleep(1000);
+  Console.Clear();
+  Console.WriteLine("Apontar...");
+  Thread.Sleep(1000);
+  Console.Clear();
+  Console.WriteLine("Vai...");
+  Thread.Sleep(1000);
+
+  Start(time);
+}
 
 static void Start(int time){
 
@@ -13,4 +52,9 @@ static void Start(int time){
     Console.Write(currentTime);
     Thread.Sleep(1000);
   }
-};
+
+  Console.Clear();
+  Console.WriteLine("O Contador foi finalizado");
+  Thread.Sleep(2500);
+  Menu();
+}
